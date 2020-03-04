17d10a058360d66e62e2b9656c95fc8990764b25
47karimasif <karimasif98@gmail.com>
2020-03-04 15:44:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// hamburger menu
+const toggleCollapse = document.querySelector(".toggle-collapse span");
+const navbar = document.querySelector(".navbar");
+
+toggleCollapse.onclick = () => {
+  navbar.classList.toggle("collapse");
+};
+
+// animation
+AOS.init();
+
+// Navbar smooth scroll
+$(".navbar a, .scrollDown").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top
+    },
+    700
+  );
+  return false;
+});
+
+// preloader
+// window.addEventListener("load", function() {
+//   setTimeout(() => {
+//     var preloader = document.getElementById("loading");
+//     preloader.style.display = "none";
+//   }, 3000);
+// });
+
+// for disabling right click
+// $(document).bind("contextmenu", function(e) {
+//   e.preventDefault();
+// });
+// $(document).keydown(function(e) {
+//   if (e.which === 123) {
+//     return false;
+//   }
+// });
