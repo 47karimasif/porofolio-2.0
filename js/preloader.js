67d770f9408ed51b89941774f579d9eacd70e9f4
47karimasif <karimasif98@gmail.com>
2020-03-04 23:59:48 +0530

    loader

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,13 @@
 // preloader
-window.addEventListener("load", function() {
+// window.addEventListener("load", function() {
+//   setTimeout(() => {
+//     var preloader = document.getElementById("loading");
+//     preloader.style.display = "none";
+//   }, 3000);
+// });
+function loadFunction() {
   setTimeout(() => {
     var preloader = document.getElementById("loading");
     preloader.style.display = "none";
   }, 3000);
-});
+}
